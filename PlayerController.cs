fc62f77af7e3bc470a488cba4ffc84a5b88988ee
Alexandre Silveira <66680378+Ale10xp@users.noreply.github.com>
2021-08-01 08:19:08 -0300

    Add files via upload
    The two scripts that works together

---
+++ PlayerController.cs
@@ -0,0 +1,222 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    public Transform lantern;
+
+    private GameObject cam;
+    private Rigidbody rb;
+    private Transform LookPoint;
+
+    public float speed = 2f;
+    public float jumpforce = 10;
+    [Range(1,5)] public float runSpeedMultiplier;
+    private float startspeed;
+    private Vector2 MoveVelocity;
+
+
+    public CharacterController characterController;
+    Vector3 velocity;
+    bool isGrounded;
+
+    public float gravity = -10f;
+    public float jumpHeight = 5f;
+
+    private float startHeight,actualHeight;
+    private float startCenterY, actualCenterY;
+    private float startRadius, actualRadius;
+    private float turnSmoothVelocity;
+
+    [Range(0,0.2f)]public float turnSmoothTime = 0.1f;
+    public Animator anim;
+
+    void Start () {
+
+        cam = GameObject.FindWithTag("MainCamera");
+        rb = GetComponent<Rigidbody>();
+
+        LookPoint = GameObject.Find("LookPoint").GetComponent<Transform>();
+        startspeed = speed;
+
+        startHeight = characterController.height;
+        startCenterY = characterController.center.y;
+        startRadius = characterController.radius;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        actualHeight = characterController.height;
+        actualCenterY = characterController.center.y;
+        actualRadius = characterController.radius;
+
+        isGrounded = characterController.isGrounded;
+
+        if (isGrounded) { anim.SetBool("isJumping", false); anim.SetFloat("AirMode 0", 0f); }
+        else anim.SetFloat("AirMode 0", 1f);
+
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        Vector3 camRotation = new Vector3(0, cam.transform.eulerAngles.y, 0);
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+        //andando
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camRotation.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+
+
+            anim.SetBool("isWalking", true);
+
+
+
+
+            Vector3 moveDir = Quaternion.Euler(0, targetAngle,0) * Vector3.forward;
+
+        characterController.Move(moveDir.normalized * speed * Time.deltaTime);
+
+
+        }
+        else anim.SetBool("isWalking", false);
+
+
+
+        
+
+
+
+
+        /*
+
+            transform.Translate(new Vector3(MoveVelocity.x, 0, MoveVelocity.y), Space.Self);
+            */
+
+
+
+        //Jump
+        if (Input.GetButton("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            anim.SetTrigger("TakeOff");
+            anim.SetBool("isJumping", true);
+        }
+
+        //Gravity
+        velocity.y += gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
+
+        //Run
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = startspeed * runSpeedMultiplier;
+
+            if (direction != new Vector3(0, 0, 0))
+            {
+                anim.SetBool("isRunning", true);
+            }
+            else anim.SetBool("isRunning", false);
+        }
+        else {
+
+            speed = startspeed;
+            anim.SetBool("isRunning", false);
+        }
+
+        speed = Mathf.Clamp(speed, speed, startspeed * runSpeedMultiplier);
+
+
+        //AGACHAR
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            anim.SetBool("isCounching", true);
+
+            //diminuir speed
+            speed = startspeed / 2;
+            speed = Mathf.Clamp(speed, startspeed/2, speed);
+
+            //abaixar altura
+            actualHeight = characterController.height / 2;
+            actualHeight = Mathf.Clamp(actualHeight, startHeight / 2, startHeight);
+
+            //abaixarCentro
+            float centerY = characterController.center.y - startHeight / 4;
+            centerY = Mathf.Clamp(centerY, startCenterY - startHeight/4  , startCenterY );
+            actualCenterY = centerY;
+
+            //aumentar Radius
+            actualRadius = Mathf.Lerp(actualRadius, startRadius*2, 5f*Time.deltaTime);
+            actualRadius = Mathf.Clamp(actualRadius, startRadius, startRadius * 2);
+            characterController.radius = actualRadius;
+        }
+        else
+        {
+            if (!CheckPlafformerUp())
+            {
+                actualHeight = startHeight;
+                actualCenterY = startCenterY;
+                actualRadius = startRadius;
+                anim.SetBool("isCounching", false);
+            }
+        }
+
+
+        characterController.center = new Vector3(characterController.center.x, actualCenterY, characterController.center.z);
+        characterController.height = actualHeight;
+        characterController.radius = actualRadius;
+        // Ligar/Desligar Lanterna
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+
+            bool stateLantern = lantern.gameObject.activeSelf;
+
+            stateLantern = !stateLantern;
+
+            lantern.gameObject.gameObject.SetActive(stateLantern);
+
+        }
+
+
+        
+        
+
+    }
+
+
+    public bool CheckPlafformerUp()
+    {
+        RaycastHit hitInfo;
+        Physics.Raycast(transform.position, Vector3.up, out hitInfo, 2f);
+
+        if (hitInfo.collider)
+        {
+
+            return true;
+        }
+        else { return false;  }
+        
+    }
+
+    public void DoPickUpAnimation()
+    {
+
+        anim.SetTrigger("PickUp");
+
+    }
+
+
+
+
+}
---
+++ RPGCameraControl.cs
@@ -0,0 +1,229 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class RPGCameraControl : MonoBehaviour {
+    [Header("Target")][Tooltip("Target to look around")]public Transform Target;
+    private Transform NowAround;
+        [Header("Sensitivity")]
+    [Tooltip("The Sensivity of View")] public float  ViewSensitivity;
+    [Tooltip("The Sensivity of Zoom")] public float ScrollSensitivity;
+        [Header("Zoom")] 
+    [Tooltip("Minimum distance of the Camera to Target")] public float MinDistance;
+    [Tooltip("Maximum distance of the Camera to Target")] public float MaxDistance;
+    [Tooltip("Some Adjustments in View")] public Vector3 Offset;
+        [Header("Angle of View")]
+    [Tooltip("Minimum Angle of view")] public float MinYAngle = -45;
+    [Tooltip("Maximum Angle of view")] public float MaxYAngle = 80;
+    
+    private float currentX = 0f, currentY = 1.5f, distance = 10f, realMinDistance, realMaxDistance, realScrollSensitivity;
+
+
+        [Header("Mouse Button")]
+    [Tooltip("Enable/Disable Left drag Click")]public bool LeftMouseButtonClickToControl; 
+    [Tooltip("Enable/Disable Right drag Click")]public bool RightMouseButtonClickToControl;
+    [Header("Details")]
+    public bool hideCursor; 
+    [Tooltip("Enable/Disable the proportional switches of values by the Medium Scale of the Target")]
+    public bool SwitchWithScale;
+    [Range(0.001f, 1)]
+    [Tooltip("Smooth Modifier")] public float SmoothSpeed = .125f;
+    private bool stateCursor;
+
+    public bool CanMoveCamera = true;
+
+    private void Start()
+    {
+        stateCursor = !hideCursor;
+
+        if (SwitchWithScale)
+            ChooseTarget();
+
+        else {
+            realMinDistance = MinDistance;
+            realMaxDistance = MaxDistance;
+            realScrollSensitivity = ScrollSensitivity;
+        }
+
+    }
+    void Update()
+    {
+        if (CanMoveCamera)
+        {
+            MouseControl();
+        }
+
+        transform.position = SmoothedPosition();
+
+        transform.LookAt(Target.position + Offset);
+
+        if (Target != NowAround) ChooseTarget();
+
+    }
+
+
+
+
+
+    private void MouseControl()
+    {
+        distance -= Input.GetAxis("Mouse ScrollWheel") * realScrollSensitivity;
+
+        distance = Mathf.Clamp(distance, realMinDistance, realMaxDistance);
+
+
+
+        if (LeftMouseButtonClickToControl)
+        {
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                currentX += Input.GetAxis("Mouse X") * ViewSensitivity;
+                currentY -= Input.GetAxis("Mouse Y") * ViewSensitivity;
+
+                currentY = Mathf.Clamp(currentY, MinYAngle, MaxYAngle);
+
+            }
+
+            if (hideCursor)
+            {
+
+
+
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    stateCursor = !stateCursor;
+                }
+
+                Cursor.visible = stateCursor;
+
+                if (!stateCursor)
+                    Cursor.lockState = CursorLockMode.Locked;
+            }
+
+
+        }
+
+        if (RightMouseButtonClickToControl)
+        {
+            if (Input.GetKey(KeyCode.Mouse1))
+            {
+                currentX += Input.GetAxis("Mouse X") * ViewSensitivity;
+                currentY -= Input.GetAxis("Mouse Y") * ViewSensitivity;
+
+                currentY = Mathf.Clamp(currentY, MinYAngle, MaxYAngle);
+
+            }
+
+            if (hideCursor)
+            {
+
+
+
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    stateCursor = !stateCursor;
+                }
+
+                Cursor.visible = stateCursor;
+
+                if (!stateCursor)
+                    Cursor.lockState = CursorLockMode.Locked;
+            }
+
+        }
+
+
+        if (RightMouseButtonClickToControl == false && LeftMouseButtonClickToControl == false)
+        {
+
+            currentX += Input.GetAxis("Mouse X") * ViewSensitivity;
+            currentY -= Input.GetAxis("Mouse Y") * ViewSensitivity;
+
+            currentY = Mathf.Clamp(currentY, MinYAngle, MaxYAngle);
+
+            if (hideCursor)
+            {
+
+
+
+                if (Input.GetKeyDown(KeyCode.Escape))
+                    stateCursor = !stateCursor;
+                    
+
+                if (stateCursor==false)
+                {
+                    Cursor.lockState = CursorLockMode.Locked;
+                    Cursor.visible = false;
+                }
+
+                else
+                {
+                    Cursor.lockState = CursorLockMode.None;
+                    Cursor.visible = true;
+
+                }
+
+            }
+
+        }
+
+    }
+
+
+    public Vector3 SmoothedPosition()
+    {
+        Vector3 dir = new Vector3(Offset.x,Offset.y, -distance + Offset.z);
+
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+
+         Vector3 desiredPosition = Target.position + (rotation * dir) ;
+
+
+        Vector3 SmoothedPosition = Vector3.Lerp(transform.position, desiredPosition, SmoothSpeed);
+
+        return SmoothedPosition;
+
+    }
+    
+
+
+
+
+    public void ChooseTarget()
+    {
+        if (SwitchWithScale)
+        {
+            float LossyScaleMedium = (Target.lossyScale.x + Target.lossyScale.y + Target.lossyScale.z) / 3f;
+            realMinDistance = MinDistance * LossyScaleMedium;
+            realMaxDistance = MaxDistance * LossyScaleMedium;
+            realScrollSensitivity = ScrollSensitivity * LossyScaleMedium;
+        }
+
+        NowAround = GetComponent<RPGCameraControl>().Target;
+
+    }
+
+
+    public void CheckRayCollison()
+    {
+        float newDistance = transform.position.magnitude;
+
+        Vector3 dir2target = (Target.position - transform.position);
+
+        Ray ray = new Ray(transform.position, dir2target);
+
+        RaycastHit hit;
+
+        if (Physics.Linecast(transform.position, Target.position, out hit))
+        {
+            transform.position = SmoothedPosition() + new Vector3(0, 0, -(hit.distance*1.2f));
+        }
+
+ 
+
+        Debug.DrawRay(transform.position, dir2target, Color.yellow);
+
+
+
+    }
+
+}
